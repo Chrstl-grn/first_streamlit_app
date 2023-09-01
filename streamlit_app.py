0c2f6e9a43e7b1a8c95f1b656b17684b229b4573
Chrstl-grn <143773530+Chrstl-grn@users.noreply.github.com>
2023-09-01 12:09:28 +0800

    Update streamlit_app.py
    corrected the error in line 8

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ streamlit.header('📃Breakfast Favorites')
 streamlit.text('🍲Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐓Hard-boiled Free-Range Egg')
-streamlit.text(🥑🍞Avocado Toast')
+streamlit.text('🥑🍞Avocado Toast')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
